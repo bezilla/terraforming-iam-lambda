570db406e39c109df0177793c4d1ed1894c15442
PJ Bezilla <bezilla@gmail.com>
2019-09-03 13:37:04 -0700

    Making Role Credentials Prettier

--- lambda_function.rb
+++ lambda_function.rb
@@ -9,6 +9,11 @@ require 'json'
 
 ROLE_ARN = "arn:aws:iam::157385605725:role/lambda-terraform"
 ROLE_SESSION_NAME = "lambda-session"
+ROLE_CREDENTIALS = Aws::AssumeRoleCredentials.new(
+    client: Aws::STS::Client.new,
+    role_arn: "#{ROLE_ARN}",
+    role_session_name: "#{ROLE_SESSION_NAME}"
+  )
 
 def lambda_handler(event:, context:)
   role_credentials = Aws::AssumeRoleCredentials.new(
@@ -18,7 +23,7 @@ def lambda_handler(event:, context:)
   )
   Aws.config.update({
   region: 'us-east-1',
-  credentials: role_credentials
+  credentials: "#{ROLE_CREDENTIALS}"
   })  
   generate_tf_files.each{|f|upload_tf_file_s3(f)}
   raise 'something went wrong' if $?.exitstatus != 0
@@ -41,7 +46,7 @@ def upload_tf_file_s3(file)
     role_arn: "#{ROLE_ARN}",
     role_session_name: "#{ROLE_SESSION_NAME}"
   )
-  s3 = Aws::S3::Resource.new(region: 'us-east-1', credentials: role_credentials)
+  s3 = Aws::S3::Resource.new(region: 'us-east-1', credentials: "#{ROLE_CREDENTIALS}")
   obj = s3.bucket('plutolegacy-iam-backups').object(file)
   obj.upload_file(File.expand_path(file, "/tmp"))
   raise 'something went wrong' if $?.exitstatus != 0
